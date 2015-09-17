cc8507e2fc2fac16f0f9eb5eb2d7f0ffb079e4f8
ryan <ryanjbrackney@gmail.com>
2015-09-17 15:17:23 -0700

    Got minimum needed functionality out of Flask, including figure display

--- front_end/app/a_Model.py
+++ front_end/app/a_Model.py
@@ -4,10 +4,20 @@ Created on Thu Sep 17 12:29:31 2015
 
 @author: ryan
 """
-def ModelIt(fromUser  = 'Default', population = 0):
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def modelit(fromUser  = 'Default', population = 0):
     print 'The population is %i' % population
     result = population/1000000.0
     if fromUser != 'Default':
         return result
     else:
         return 'check your input'
+
+def graph_prices(time_stamps, iprices,fprices,gname):
+    
+    x = np.arange(1,10)
+
+    
--- front_end/app/views.py
+++ front_end/app/views.py
@@ -4,7 +4,7 @@ Created on Thu Sep 17 10:55:30 2015
 
 @author: ryan
 """
-from flask import render_template, request
+from flask import render_template, request, make_response
 from app import app
 import os
 import pymysql as mdb
@@ -77,7 +77,7 @@ def cities_output():
   
   #call a function from a_Model package. note we are only pulling one result in the query
   pop_input = cities[0]['population']   
-  the_result = a_Model.ModelIt(city, pop_input)
+  the_result = a_Model.modelit(city, pop_input)
   return render_template("output.html", cities = cities, the_result = the_result)
   
 @app.route('/ssoutput')
@@ -92,4 +92,33 @@ def ss_output():
         
     game_info = str(query_results[0]) #just return the first option
     return render_template("ssoutput.html", game_info = game_info)
-    
+
+@app.route("/priceplot.png")
+def priceplot():
+    '''modified from example at https://gist.github.com/wilsaj/862153'''
+    import datetime
+    import StringIO
+    import random
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
